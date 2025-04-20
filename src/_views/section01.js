34f637848b57173c54165c4fddd26e5d3d410f8f
kim2gon <heegonkim2001@gmail.com>
2025-04-20 12:53:47 +0900

    12312321

--- src/_views/section01.js
+++ src/_views/section01.js
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useRef } from "react";
+import { motion, useScroll, useTransform } from 'framer-motion';
 
 const Section01 = () => {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+  });
   return (
     <section className="w-full h-[400vh] relative bg-black text-white">
       <div className="sticky top-0 h-screen flex items-center justify-center">
@@ -8,18 +13,23 @@ const Section01 = () => {
           style={{ backgroundImage: `url(/digivice.jpg)` }}
           className="w-[400px] h-[400px] flex justify-center items-center"
         >
-          <img
-            src={`/001/egg00.png`}
-            alt={`egg 00`}
-            className="max-w-full max-h-full"
-          />
+          {['egg00', 'egg01', 'egg02', 'egg03', 'egg04', 'egg05',
+            'egg06', 'egg07', 'egg08', 'egg09', 'egg10', 'egg11'
+          ].map((v, i) => (
+            <img key={v}
+              src={`/001/${v}.png`}
+              alt={`${v}`}
+              className="max-w-full max-h-full"
+            />
+          ))
+          }
         </div>
       </div>
       <div className="sticky top-0 left-0 w-full h-full overflow-hidden">
-        <div className="absolute left-0 w-1/4 h-full bg-white"></div>
-        <div className="absolute left-1/4 w-1/4 h-full bg-white"></div>
-        <div className="absolute left-2/4 w-1/4 h-full bg-white"></div>
-        <div className="absolute left-3/4 w-1/4 h-full bg-white"></div>
+        <div className="absolute left-0 w-1/4 h-full bg-white" />
+        <div className="absolute left-1/4 w-1/4 h-full bg-white" />
+        <div className="absolute left-2/4 w-1/4 h-full bg-white" />
+        <div className="absolute left-3/4 w-1/4 h-full bg-white" />
       </div>
     </section>
   );
